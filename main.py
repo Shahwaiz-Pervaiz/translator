6f19560f4eccf31690258915d492001121dd9906
Shahwaiz <shahwaizshezii@gmail.com>
2025-04-25 23:32:40 -0700

    initial

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("HEllo WOrld")
+from translate import Translator
+with open("read.txt","r") as file:
+    text=file.read()
+    translator = Translator(to_lang="ur")
+    translation = translator.translate(text)
+with open("write.txt","w",encoding="utf-8") as file:
+     file.write(translation)
+
